08e50ad52334d69cb943e700fef05db81d54881e
Abhishek Singh <62643405+Abhishekfm@users.noreply.github.com>
2022-12-31 08:24:44 +0000

    Route Auth

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express()
 
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const userRoute = require("./routes/user.route")
 
 app.use(express.json())
+
 app.use(express.urlencoded({
     extended:true
 }))
+
 app.use(cookieParser())
+
 app.use(cors())
+
+app.use("/auth", userRoute)
 
 module.exports = app;
---
+++ routes/user.route.js
@@ -0,0 +1,12 @@
+const {createUser, login, logout} = require("../controllers/user.controller")
+const express = require("express")
+const express = require("express")
+
+const router = express.Router()
+
+
+router.post("/createuser",createUser)
+router.post("/login",login)
+router.post("/logout",logout)
+
+module.exports = router
