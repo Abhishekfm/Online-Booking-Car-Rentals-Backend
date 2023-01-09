ac5af61d3db715b54ee8b81f1b6e0a3d88c0c288
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2023-01-09 22:39:35 +0530

    post to get and get to post

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -5,10 +5,10 @@ const express = require("express")
 const router = express.Router()
 
 router.post("/createcar", isAdminLoggedIn, createCar)
-router.post("/addcaratsamelocation/:carId", isAdminLoggedIn, addCarAtSameLocation)
-router.post("/decrementcaratsamelocation/:carId", isAdminLoggedIn, decrementCarAtSameLocation)
+router.get("/addcaratsamelocation/:carId", isAdminLoggedIn, addCarAtSameLocation)
+router.get("/decrementcaratsamelocation/:carId", isAdminLoggedIn, decrementCarAtSameLocation)
 router.delete("/deletecar/:carId", isAdminLoggedIn, deleteCar)
-router.get("/showcardb", isAdminLoggedIn, showCarDb)
+router.post("/showcardb", isAdminLoggedIn, showCarDb)
 router.get("/showorderdb", isAdminLoggedIn, showOrderDb)
 
 module.exports = router
